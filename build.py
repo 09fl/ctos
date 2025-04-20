2b10416ac47a44ee3b7503e2b290c78fd05c7e4b
09fl <6722817+09fl@users.noreply.github.com>
2025-04-20 21:55:21 +0900

    parse markdown in build script

--- build.py
+++ build.py
@@ -2,28 +2,26 @@ from pathlib import Path
 from shutil import copy2, copytree, rmtree
 import json
 import re
+import mistletoe
 
 CTOS_DIR = 'ctos'
 OUTPUT_DIR = 'public'
 CONTENT_DIR = 'content'
 EXTRA_COPIES = ['favicon.png']
-ACTION_TABLE = {
-    '.': {
+HTML_EXTENSIONS = {'.html', '.md'}
+COPY_EXTENSIONS = {'.jpg', '.png'}
+DEFAULT_METADATA = {
+    'folder': {
         'type': 'folder',
         'icon': 'folder',
         'style': 'medium'
     },
-    '.html': {
-        'type': 'url',
+    'html': {
+        'type': 'html',
         'icon': 'text',
         'style': 'large'
     },
-    '.md': {
-        'type': 'md',
-        'icon': 'text',
-        'style': 'large'
-    },
-    'index.html': {
+    'iframe': {
         'type': 'iframe',
         'icon': 'exe',
         'style': 'large'
@@ -80,7 +78,7 @@ def process_dir(path, node):
                     'file': '/'+str(child),
                     'path': '/'+str(child.relative_to(CONTENT_DIR))
                 }
-                node[name].update(ACTION_TABLE['index.html'])
+                node[name].update(DEFAULT_METADATA['iframe'])
                 node[name].update(parse_metadata(child/'index.html'))
 
                 copytree(child, Path(OUTPUT_DIR)/child)
@@ -93,32 +91,40 @@ def process_dir(path, node):
                     'file': {},
                     'path': '/'+str(child.relative_to(CONTENT_DIR))
                 }
-                node[name].update(ACTION_TABLE['.'])
+                node[name].update(DEFAULT_METADATA['folder'])
                 if (child/'metadata').exists():
                     node[name].update(parse_metadata(child/'metadata'))
                 process_dir(child, node[name]['file'])
 
-        elif child.suffix in ACTION_TABLE:
+        elif child.suffix in HTML_EXTENSIONS:
             # Single file
-            print('File: {}'.format(child))
+            print('Html: {}'.format(child))
 
             node[name] = {
                 'name': name,
-                'file': '/'+str(child.with_suffix('')) if child.suffix == '.html' else '/'+str(child),
+                'file': '/'+str(child.with_suffix('')),
                 'path': '/'+str(child.relative_to(CONTENT_DIR).with_suffix(''))
             }
-            node[name].update(ACTION_TABLE[child.suffix])
+            node[name].update(DEFAULT_METADATA['html'])
             node[name].update(parse_metadata(child))
 
+            # Parse markdown if needed
+            if child.suffix == '.md':
+                with open(child, 'r') as src:
+                    rendered = mistletoe.markdown(src)
+                with open((Path(OUTPUT_DIR)/child).with_suffix('.html'), 'w') as dst:
+                    dst.write(rendered)
+            else:
+                copy2(child, Path(OUTPUT_DIR)/child)
+        elif child.suffix in COPY_EXTENSIONS:
+            print('Copy: {}'.format(child))
             copy2(child, Path(OUTPUT_DIR)/child)
-        else:
-            continue
 
         # Add link and startup
-        if 'link' in node[name]:
+        if name in node and 'link' in node[name]:
             print('Link: {}'.format(node[name]['path']))
             output['link'][node[name]['link']] = node[name]['path']
-        if 'startup' in node[name]:
+        if name in node and 'startup' in node[name]:
             print('Startup: {}'.format(node[name]['path']))
             output['startup'][node[name]['startup']] = node[name]['path']
 
